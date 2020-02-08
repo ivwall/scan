2f9ea18c11583890e0d18986893df732731bf518
cf2141 <cf2141@tgndomains.com>
2020-02-08 08:52:49 -0500

    started parsing the test file

--- scan/src/main/java/dev/App.java
+++ scan/src/main/java/dev/App.java
@@ -1,6 +1,11 @@
 package dev;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+//import org.apache.commons.io.*;
 
 public class App {
   
@@ -21,7 +26,41 @@ public class App {
   
   public void parseFile(String f) {
     try {
+      
+      BufferedReader reader = new BufferedReader(new FileReader(f));
+      String line = reader.readLine();
+      if (line != null) {
+        System.out.println(line);
+        
+        int xIndex = line.indexOf('x');
+        
+        System.out.println(" xIndex "+xIndex);
+        
+        System.out.println(line.substring(0,xIndex));
+        
+        String rStr = line.substring(0,xIndex);
+        
+        int r = Integer.parseInt(rStr);
+        
+        System.out.println(" r = "+r);
+        
+        String cStr = line.substring(xIndex+1,line.length());
+        
+        System.out.println(" cStr "+cStr);
+        
+        int c = Integer.parseInt(cStr);
+        
+        if ( r != c ) {
+          System.out.println("need to raise an error here r "+r+" != "+c);
+        }
+        
+      }
+      
+      
+      
+      
     } catch ( Exception ex ) {
+      ex.printStackTrace();
     }
   }
   
